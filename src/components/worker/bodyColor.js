1dc78ff175900c2d9371a81bdc4106cecde8798f
ikkikki <reformplan.sudou@gmail.com>
2020-09-18 10:44:50 +0900

    milestone
    body color change

--- src/components/worker/bodyColor.js
+++ src/components/worker/bodyColor.js
@@ -1,38 +1,38 @@
 export default {
   1: {
     bodykashin: '天 空',
-    bodyColor: 'radial-gradient(silver 40%, skyblue)',
+    bodyColor: 'radial-gradient(white, azure, whitesmoke)',
   },
   2: {
     bodykashin: '火の神',
-    bodyColor: 'radial-gradient(darkorange 20%, red 45%, magenta)',
+    bodyColor: 'radial-gradient(red, deeppink, orangered)',
   },
   3: {
     bodykashin: '風 神',
-    bodyColor: 'radial-gradient(orange, greenyellow, lightgreen)',
+    bodyColor: 'radial-gradient(palegreen, honeydew, ivory)',
   },
   4: {
     bodykashin: '雷 神',
-    bodyColor: 'radial-gradient(gold 35%, yellow)',
+    bodyColor: 'radial-gradient(gold, yellow, lemonchiffon)',
   },
   5: {
     bodykashin: '山の神',
-    bodyColor: 'radial-gradient(saddlebrown, darkgreen)',
+    bodyColor: 'radial-gradient(darkgreen, darkolivegreen, saddlebrown)',
   },
   6: {
     bodykashin: '聖 湖',
-    bodyColor: 'radial-gradient(mediumorchid 35%, violet 60%, lightpink)',
+    bodyColor: 'radial-gradient(lavenderblush, violet, mediumpurple)',
   },
   7: {
     bodykashin: '水 神',
-    bodyColor: 'radial-gradient(indigo 45%, blue)',
+    bodyColor: 'radial-gradient(blue, darkturquoise, cyan)',
   },
   8: {
     bodykashin: '天 空',
-    bodyColor: 'radial-gradient(silver 40%, skyblue)',
+    bodyColor: 'radial-gradient(white, azure, whitesmoke)',
   },
   9: {
     bodykashin: '大 地',
-    bodyColor: 'radial-gradient(black 20%, cornsilk)',
+    bodyColor: 'radial-gradient(burlywood, peru, black)',
   },
 };
