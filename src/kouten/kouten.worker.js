52be2c124f418f9b429d6ed7412aec0ceec60b30
ikkikki <reformplan.sudou@gmail.com>
2020-10-03 16:42:20 +0900

    milestone
    daijyun change

--- src/kouten/kouten.worker.js
+++ src/kouten/kouten.worker.js
@@ -183,7 +183,6 @@ export const genDaijyunArr = ({ seinen, seibetu }) => {
   const day = date.getDate();
   // console.log(month, day);
   // console.log(inyo);
-  const tukijyunsu = genTukijyunsu({ seinen });
   // console.log('tuki', tukijyunsu);
   const jyunsetu = inyo ? junsetuYou : junsetuIn;
   const setusu = Number(jyunsetu[month][day]);
@@ -193,8 +192,10 @@ export const genDaijyunArr = ({ seinen, seibetu }) => {
       ? setusu + (k - 1) * 10 : 0} ~ ${setusu - 1 + k * 10}歳`;
     return res === '0 ~ 0歳' ? '0歳' : res;
   });
-  // console.log(setusu, jyunsetu);
-  return setuArr;
+  const tukiArr = genGetuArr();
+  const newArr = tukiArr.map((i) => setuArr[i - 1]);
+  // console.log('nn', newArr);
+  return newArr;
 };
 
 const genGetuRevArr = () => {
