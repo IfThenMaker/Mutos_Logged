f92a48c678b372c4b989a588a1ee613efbfc56e3
ikkikki <reformplan.sudou@gmail.com>
2020-10-07 11:22:54 +0900

    update
    input gender

--- src/dialog/entry.textField.seibetu.jsx
+++ src/dialog/entry.textField.seibetu.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 
-const SexField = ({ dispatch }) => (
-  <TextField
-    autoFocus
-    required
-    margin="dense"
-    id="sex"
-    label="性別"
-    type="text"
-    fullWidth
-    onChange={(e) => dispatch(e.target.value)}
-  />
-);
+const useStyles = makeStyles({
+  gender: {
+    display: 'flex',
+    flexDirection: 'row',
+    // justifyContent: 'center',
+    margin: '15px 0px 10px 0',
+  },
+});
+
+const SexField = ({ dispatch }) => {
+  const classes = useStyles();
+  const [value, setValue] = useState('male');
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    dispatch(e.target.value);
+  };
+
+  return (
+    <RadioGroup
+      className={classes.gender}
+      aria-label="gender"
+      name="gender1"
+      value={value}
+      onChange={handleChange}
+    >
+      <FormControlLabel value="male" control={<Radio />} label="男性" />
+      <FormControlLabel value="female" control={<Radio />} label="女性" />
+    </RadioGroup>
+  );
+};
 SexField.defaultProps = {
   dispatch: (e) => e,
 };
